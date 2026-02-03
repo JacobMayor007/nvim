0891081d83b2a2d82d25ff92409300d590a8b732
JacobMayor007 <jacobmary.tapere007@gmail.com>
2026-02-03 18:18:31 +0800

    autocomplete commit

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- 1. BOOTSTRAP LAZY.NVIM (The Plugin Manager)
+-- 1. BOOTSTRAP LAZY.NVIM
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -8,21 +8,64 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- 2. INSTALL PLUGINS
+-- 2. INSTALL PLUGINS (One single setup call)
 require("lazy").setup({
-  "neovim/nvim-lspconfig", -- Required for your Go Auto-import code
-  "williamboman/mason.nvim", -- The "App Store" for servers
-  "williamboman/mason-lspconfig.nvim", -- Bridges Mason and Lspconfig
+  "neovim/nvim-lspconfig",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  { "NvChad/nvim-colorizer.lua", opts = { user_default_options = { tailwind = true } } },
+
+  -- --- AUTOCOMPLETE PLUGINS ---
+  "hrsh7th/nvim-cmp",         -- The engine that shows the popup menu
+  "hrsh7th/cmp-nvim-lsp",     -- Gets suggestions from your Go/React LSPs
+  "hrsh7th/cmp-buffer",       -- Gets suggestions from the current file
+  "hrsh7th/cmp-path",         -- Gets suggestions for file paths
+  "saadparwaiz1/cmp_luasnip", -- Connects snippets to the menu
+  {
+    "L3MON4D3/LuaSnip",       -- The snippet engine
+    dependencies = { "rafamadriz/friendly-snippets" }, -- The "Library" of actual snippets
+    config = function()
+      -- This line loads the library (React, Go, etc.)
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
 })
 
--- 3. SETUP THE GO BRAIN (gopls)
+-- 3. MASON & LSP CONFIGURATION
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "gopls" }
+    -- Added the web servers here so Mason installs them automatically!
+    ensure_installed = { "gopls", "vtsls", "tailwindcss", "emmet_language_server" }
 })
-require("lspconfig").gopls.setup({})
 
--- 4. YOUR AUTO-IMPORT LOGIC
+-- GO Setup
+vim.lsp.config("gopls", {
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_markers = { "go.work", "go.mod", ".git" },
+})
+vim.lsp.enable("gopls")
+
+-- React & Node.js (vtsls)
+vim.lsp.config("vtsls", {
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  root_markers = { "package.json", "tsconfig.json", ".git" },
+})
+vim.lsp.enable("vtsls")
+
+-- Tailwind CSS
+vim.lsp.config("tailwindcss", {
+  root_markers = { "tailwind.config.js", "tailwind.config.ts", "package.json" },
+})
+vim.lsp.enable("tailwindcss")
+
+-- Emmet
+vim.lsp.config("emmet_language_server", {
+  filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
+})
+vim.lsp.enable("emmet_language_server")
+
+-- 4. AUTO-COMMANDS (Go Auto-import)
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
@@ -45,7 +88,9 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 -- 5. SETTINGS & KEYMAPS
 vim.opt.number = true
 vim.opt.mouse = 'a'
-vim.opt.clipboard = 'unnamedplus' -- Makes Ctrl+C/V work with Windows!
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.wildmenu = true
+vim.opt.wildmode = "longest:list,full"
 
 -- Save with Ctrl + S (All modes)
 vim.keymap.set({'n', 'i', 'v'}, '<C-s>', '<Cmd>w<CR>', { silent = true })
@@ -53,6 +98,33 @@ vim.keymap.set({'n', 'i', 'v'}, '<C-s>', '<Cmd>w<CR>', { silent = true })
 -- Terminal Escape
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { desc = 'Exit terminal mode' })
 
--- Folder filtering
-vim.opt.wildmenu = true
-vim.opt.wildmode = "longest:list,full"
+-- 6. AUTOCOMPLETE SETUP
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(), -- Trigger menu manually
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Enter to select
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' }, -- Sugestions from gopls, vtsls, etc.
+    { name = 'luasnip' },  -- Snippets (like log -> console.log)
+    { name = 'buffer' },   -- Text from current file
+    { name = 'path' },     -- File paths
+  })
+})

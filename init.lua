72d0a1594918c458560e10b5f7e12b0b314eaf49
JacobMayor007 <jacobmary.tapere007@gmail.com>
2026-02-17 11:24:38 +0800

    added the highlights, colors, every keyword of the programming language

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- This is working fine
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
@@ -16,6 +17,13 @@ require("lazy").setup({"neovim/nvim-lspconfig", {
     "williamboman/mason.nvim",
     config = function()
         require("mason").setup()
+    end
+}, {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd([[colorscheme tokyonight-night]])
     end
 }, {
     "williamboman/mason-lspconfig.nvim",
